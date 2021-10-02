5ef87cb1e2087052a7ef49f9818bc05bca2e2582
Chandrika Deb <chandrikadeb7@gmail.com>
2021-10-02 16:39:10 +0530

    Create Floyd_Triangle.cpp

---
+++ CPP/Floyd_Triangle.cpp
@@ -0,0 +1,13 @@
+public class floydTriangle {
+    public static void main(String[] args){
+        int num=1;
+        for(int i=1;i<=5;i++)
+        {
+            for(int j=1;j<=i;j++){
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+}
