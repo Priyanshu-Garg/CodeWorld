c23e68c2237c016e13e084844bb227ead856e10b
Abhishek Yadav <70373142+abhishek351@users.noreply.github.com>
2021-10-23 14:43:26 +0530

    file added

--- Python/coin change.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def count(self, S, m, n): 
-        table = [0]*(n+1)
-        table[0] = 1
-        for i in range(0,m):
-            for j in range(S[i],n+1):
-                table[j] += table[j-S[i]]
-        return table[n] 
---
+++ Python/reverse linked list.py
@@ -0,0 +1,34 @@
+
+def reverse(head):
+    prev=None
+    curr=head
+   
+    while curr:
+        nxt=curr.next
+        curr.next=prev
+        prev=curr
+        curr=nxt
+    return prev
+    
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    tests = int(input())
+
+    for tests_itr in range(tests):
+        llist_count = int(input())
+
+        llist = SinglyLinkedList()
+
+        for _ in range(llist_count):
+            llist_item = int(input())
+            llist.insert_node(llist_item)
+
+        llist1 = reverse(llist.head)
+
+        print_singly_linked_list(llist1, ' ', fptr)
+        fptr.write('\n')
+
+    fptr.close()
